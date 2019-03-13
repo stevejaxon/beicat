99d9b1a66b40fdab6c9b2e857ebb937da3d877d7
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-03-13 15:19:43 -0700

    feat(game over): initial work to load a game over screen

---
+++ godot/Beicat.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+func _ready():
+	# Connect the signal for when a player falls to the bottom of the screen to the function to handle the game being over
+	$FallDetector.connect("body_entered", self, "gameOver")
+
+func gameOver() -> void:
+	get_tree().change_scene("res://interface/game_over/GameOverScreen.tscn")
