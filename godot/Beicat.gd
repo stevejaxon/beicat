3aad6d77e9bc28fef93b0df33033a7dad96a1e90
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-03-14 09:50:05 -0700

    feat(jump): out of bounds now detected correctly and player wraps around the screen

--- godot/Beicat.gd
+++ godot/Beicat.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 func _ready():
 	# Connect the signal for when a player falls to the bottom of the screen to the function to handle the game being over
-	$FallDetector.connect("body_entered", self, "gameOver")
+	assert($FallDetector.connect("area_exited", self, "gameOver") == 0)
+	assert($LeftScreenDetector.connect("area_exited", $Character, "playerLeftScreen", [screen_side.LEFT]) == 0)
+	assert($RightScreenDetector.connect("area_exited", $Character, "playerLeftScreen", [screen_side.RIGHT]) == 0)
 
-func gameOver() -> void:
-	print("game over")
+func gameOver(area: Area2D) -> void:
 	get_tree().change_scene("res://interface/game_over/GameOverScreen.tscn")
--- godot/character/Character.gd
+++ godot/character/Character.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
-const GRAVITY = 9.8
+const GRAVITY: float = 9.8
+const SCREEN_WIDTH: int = 720
 
 # The jump height is negative because down on the Y axis in games in positive, therefore, up on the y axis is negative
 export var jump_height = -10
@@ -12,11 +13,10 @@ class_name Character
 var velocity = Vector2(0,0)
 
 func _ready():
-	$EntityDetection.connect("body_entered", self, "landed_on")
+	assert($EntityDetection.connect("body_entered", self, "landed_on") == 0)
 
 func _process(delta):
 	velocity.y = velocity.y + (GRAVITY * delta)
-	print(velocity)
 	move_and_collide(velocity)
 
 func _input(event):
@@ -27,3 +27,9 @@ func _input(event):
 
 func landed_on(body) -> void:
 	velocity.y = jump_height
+
+func playerLeftScreen(area: Area2D, screenSide) -> void:
+	if screenSide == screen_side.LEFT:
+		position.x = position.x + SCREEN_WIDTH
+	elif screenSide == screen_side.RIGHT:
+		position.x = position.x - SCREEN_WIDTH
