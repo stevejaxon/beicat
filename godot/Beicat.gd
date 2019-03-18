5b0044e0dd60c828b0254139d240a3c72dda7bf0
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-03-18 15:01:21 -0700

    feat: removing the platform factory

--- godot/Beicat.gd
+++ godot/Beicat.gd
@@ -10,17 +10,6 @@ func _ready():
 	for platform in $Platforms.get_children():
 		assert($Character.connect("scroll_level", platform, "scrollDown") == 0)
 		assert($FallDetector.connect("area_entered", platform, "leftScreen") == 0)
-	_connectPlatformFactories()
 
 func gameOver(area: Area2D) -> void:
 	get_tree().change_scene("res://interface/game_over/GameOverScreen.tscn")
-
-func _connectPlatformFactories():
-	for platformFactory in $PlatformFactories.get_children():
-		assert($Character.connect("scroll_level", platformFactory, "instance") == 0)
-		assert(platformFactory.connect("new_platform", self, "_addPlatformToScene") == 0)
-
-func _addPlatformToScene(platform) -> void:
-	call_deferred("add_child", platform)
-	assert($Character.connect("scroll_level", platform, "scrollDown") == 0)
-	assert($FallDetector.connect("area_entered", platform, "leftScreen") == 0)
