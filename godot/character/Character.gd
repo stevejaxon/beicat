9e4f81e2f7a60daff1037db6a5b2c6f659c84b50
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-03-22 10:18:52 -0700

    chore: renaming a variable to make the code clearer

--- godot/character/Character.gd
+++ godot/character/Character.gd
@@ -25,8 +25,12 @@ func _process(delta):
 	var amount_jumped = 0
 	var gravity_to_apply = GRAVITY * delta
 	if remaining_jump_height < 0:
+		prints('remaining jump height before', remaining_jump_height)
 		amount_jumped = remaining_jump_height + delta
+		prints('amount jumped', amount_jumped)
 		remaining_jump_height = amount_jumped + gravity_to_apply
+		prints('remaining jump height after', remaining_jump_height)
+		
 	var new_vertical_position = position.y + gravity_to_apply + amount_jumped
 	var pointer_position = 	get_global_mouse_position()		
 	if not PLATFORM_COLLISION_DETECTOR.disabled and amount_jumped < 0:
@@ -34,7 +38,8 @@ func _process(delta):
 	elif PLATFORM_COLLISION_DETECTOR.disabled and amount_jumped >= 0:
 		_enableInteractingWithPlatforms()
 		$Sprite.play("falling")
-		
+	
+	# ensure that the animated sprite is looking in the correct direction
 	if pointer_position.x < SCREEN_MID_POINT:
 		$Sprite.flip_h = true
 	elif $Sprite.flip_h:
@@ -42,11 +47,12 @@ func _process(delta):
 
 	position = Vector2(clamp(pointer_position.x, 0, SCREEN_WIDTH), new_vertical_position)
 
-func landed_on(body: Platform) -> void:
+func landed_on(platform: Platform) -> void:
+	print('*** BOOP ***')
 	remaining_jump_height = jump_height
-	body.land()
+	platform.land()
 	$Sprite.play("jumping")
-	var platform_y_position = body.position.y
+	var platform_y_position = platform.position.y
 	var scroll_distance = SCROLL_TO_Y_POSITION - platform_y_position
 	emit_signal("scroll_level", scroll_distance, REGULAR_JUMP_SCROLL_SPEED )
 
