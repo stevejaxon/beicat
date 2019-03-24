b37d1b6a099e8924698c45c117b5c193b109c2af
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-03-24 12:38:29 -0700

    removing debug output

--- godot/Beicat.gd
+++ godot/Beicat.gd
@@ -5,9 +5,7 @@ signal level_load_completed
 func _ready():
 	# Connect the signal for when a player falls to the bottom of the screen to the function to handle the game being over
 	assert($FallDetector.connect("body_exited", self, "gameOver") == 0)
-	print('before')
 	_connect_platforms()
-	print('after')
 	assert(self.connect("level_load_completed", $CanvasLayer/StartLevel, "level_loaded") == 0)
 	emit_signal("level_load_completed")
 
