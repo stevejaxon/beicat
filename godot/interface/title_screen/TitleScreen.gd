06711339ffed74b830595a026cd014b79317c4c2
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-03-24 11:57:29 -0700

    feat(title screen): refing the animated character to the title screen

--- godot/interface/title_screen/TitleScreen.gd
+++ godot/interface/title_screen/TitleScreen.gd
@@ -1,18 +1,24 @@
 extends Node2D
 
-const IDLE_TIME_SECONDS = 10
-const WALK_TIME_SECONDS = 1
+const IDLE_TIME_SECONDS = 5
+const WALK_TIME_SECONDS = 5
 
 func _ready():
 	_animate_character()
 
 func _animate_character() -> void:
-	print('start')
 	$Cooper/Sprite.play("idle")
 	$Timer.start(IDLE_TIME_SECONDS)
 	yield($Timer, "timeout")
-	print('about to walk')
 	$Cooper/Sprite.play("walking")
 	$Timer.start(WALK_TIME_SECONDS)
 	yield($Timer, "timeout")
 	$Cooper/Sprite.play("idle")
+	yield($Cooper/Sprite, "animation_finished")
+	$Cooper/Sprite.flip_h = true
+	$Cooper/Sprite.play("walking")
+	$Timer.start(WALK_TIME_SECONDS)
+	yield($Timer, "timeout")
+	$Cooper/Sprite.flip_h = false
+	$Cooper/Sprite.play("idle")
+	$Timer.stop()
