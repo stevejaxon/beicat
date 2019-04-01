c1dac2dcba7426cb8f60096bc7a0398cdfc18316
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-04-01 10:13:58 -0700

    feat(background): improving the background scroll animation

--- godot/interface/level_background/ParallaxBackground.gd
+++ godot/interface/level_background/ParallaxBackground.gd
@@ -1,5 +1,16 @@
 extends ParallaxBackground
 
+const SCROLL_END_THRESHOLD: float = 0.1
+
+var distance_to_scroll = 0
+var scroll_velocity = 0
+
+func _process(delta):
+	if distance_to_scroll > SCROLL_END_THRESHOLD:
+		var scroll_this_tick = distance_to_scroll * delta * scroll_velocity
+		set_scroll_offset(scroll_offset + Vector2(0, scroll_this_tick))
+		distance_to_scroll = distance_to_scroll - scroll_this_tick
+
 func characterProgressedUp(distance: int, velocity: int) -> void:
-	# The character broadcasts the height it has jumped up and we want the background to scroll down (-ve in the Y axis)
-	set_scroll_offset(Vector2(0, -distance))
+	distance_to_scroll = distance
+	scroll_velocity = velocity
