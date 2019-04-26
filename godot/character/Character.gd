74d34c59b6a2d43c7c827f05bf9f01ba0f5de320
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-04-25 22:09:34 -0700

    feat(movement): modifying the movement of the character to rely more on the game engine for physics

--- godot/character/Character.gd
+++ godot/character/Character.gd
@@ -1,23 +1,23 @@
 extends KinematicBody2D
 
-const GRAVITY: float = 350.0
+const GRAVITY: float = 50.0
 const SCREEN_WIDTH: int = 720
 const SCROLL_TO_Y_POSITION: int = 1300
-const REGULAR_JUMP_SCROLL_SPEED: int = 15
-const JUMP_END_THRESHOLD: int = -75
 
 # The jump height is negative because down on the Y axis in games in positive, therefore, up on the y axis is negative
-export var jump_height = -500
+export var jump_height = -1
 # A variable for use when the character is re-used outside of the main game, e.g in the menu system
 export var in_game: bool = true
 
 class_name Character
 
-var remaining_jump_height: float = 0.0
+var speed_y: float = 0.0
+var speed_x: float = 0.0
 var previous_mouse_x_position: float = 0.0
 
 func _ready():
 	assert($PlatformDetection.connect("area_entered", self, "landed_on") == 0)
+	assert(jump_height < 0)
 	if not in_game:
 		set_process(false)
 	else:
@@ -25,31 +25,26 @@ func _ready():
 		$Sprite.play("falling")
 
 func _process(delta):
-	var amount_jumped = 0
-	var new_vertical_position = 0
-	if remaining_jump_height < JUMP_END_THRESHOLD:
-		amount_jumped = remaining_jump_height * delta
-		remaining_jump_height = remaining_jump_height - amount_jumped
-		new_vertical_position = position.y + amount_jumped
-	else:
-		remaining_jump_height = 0
-		new_vertical_position = position.y + (GRAVITY * delta)
-		
-	var pointer_position = 	get_global_mouse_position()		
-	if amount_jumped >= 0:
+	# Apply the gravity
+	speed_y += GRAVITY * delta
+	
+	if speed_y >= 0:
 		$Sprite.play("falling")
 	
+	var pointer_position = 	get_global_mouse_position()
 	# ensure that the animated sprite is looking in the correct direction
 	if pointer_position.x < previous_mouse_x_position:
 		$Sprite.flip_h = true
 	elif pointer_position.x > previous_mouse_x_position:
 		$Sprite.flip_h = false
+		
 	previous_mouse_x_position = pointer_position.x
 	
-	position = Vector2(clamp(pointer_position.x, 0, SCREEN_WIDTH), new_vertical_position)
+	# move_and_collide(Vector2(clamp(pointer_position.x, 0, SCREEN_WIDTH), speed_y))
+	move_and_collide(Vector2(speed_x, speed_y))
 
 func landed_on(platform: Platform) -> void:
 	platform.land()
 	$Sprite.play("idle")
 	$Sprite.play("jumping")
-	remaining_jump_height = jump_height
+	speed_y = jump_height
