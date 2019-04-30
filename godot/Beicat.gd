4d3aa5369c527437755cdca2b6a8dc05fed1cfa9
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-04-29 20:47:05 -0700

    feat(level load): adding better level loading

--- godot/Beicat.gd
+++ godot/Beicat.gd
@@ -7,6 +7,7 @@ const LEVELS_PER_DIFFICULTY_STAGE = 8
 
 var last_loaded_platform
 var difficulty_stage: int = 0
+var difficulty_level: int = 0
 
 func _ready():
 	# Connect the signal for when a player falls to the bottom of the screen to the function to handle the game being over
@@ -14,23 +15,33 @@ func _ready():
 	assert(self.connect("level_load_completed", $CanvasLayer/StartLevel, "level_loaded") == 0)
 	assert($CanvasLayer/StartLevel.connect("level_started", self, "_play_level_music") == 0)
 	_load_level()
-	_load_level()
-	_load_level()
 	emit_signal("level_load_completed")
 
 func _load_level() -> void:
-	var platformsData = LevelLoader.load("data")
+	var platformsData = LevelLoader.load("data", difficulty_stage, difficulty_level)
 	var y_offset = 0
 	if last_loaded_platform != null:
 		y_offset = SCREEN_HEIGHT - last_loaded_platform.position.y
 	for data in platformsData:
 		data.position = Vector2(data.position.x, (data.position.y - y_offset))
 		_display_platform(data.platform, data.position)
+		_connect_platform_signals(data.platform)
 	last_loaded_platform = platformsData[platformsData.size() -1]
+	difficulty_level += 1
+	if difficulty_level == LEVELS_PER_DIFFICULTY_STAGE:
+		difficulty_stage +=1
+		difficulty_level = 0
 
 func _display_platform(platform, absolutePosition: Vector2) -> void:
 	platform.position = absolutePosition
 	$Platforms.add_child(platform)
+
+func _connect_platform_signals(platform: Platform) -> void:
+	assert(platform.connect("platform_exited", self, "_handle_platform_exit") == 0)
+
+func _handle_platform_exit(stage: int, level: int) -> void:
+	if stage == difficulty_stage && level < difficulty_level:
+		_load_level()
 
 func _play_level_music() -> void:
 	$AudioStreamPlayer.play()
--- godot/entities/platforms/Platform.gd
+++ godot/entities/platforms/Platform.gd
@@ -1,10 +1,19 @@
 extends Area2D
 
+signal platform_exited
+
+var stage: int
+var level: int
+
 class_name Platform
+
+func set_stage_and_level(_stage: int, _level: int):
+	stage = _stage
+	level = _level
 
 func land() -> void:
 	$AudioStreamPlayer.play()
+	emit_signal("platform_exited", stage, level)
 
 func remove_from_game() -> void:
 	call_deferred("queue_free")
-	print('removing')
