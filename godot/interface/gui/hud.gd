f9674b869436a7cddb8b8539fb5ddd21c810bd21
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-05-27 21:58:45 -0700

    feat(fps): adding an FPS counter to the HUD for debug purposes

--- godot/interface/gui/hud.gd
+++ godot/interface/gui/hud.gd
@@ -1,4 +1,7 @@
 extends Node
 
+func _process(delta) -> void:
+	$VBoxContainer/HBoxContainer2/FPSValue.set_text(String(Engine.get_frames_per_second()))
+
 func updateScore() -> void:
 	$VBoxContainer/HBoxContainer/ScoreValue.set_text(String(global_variables.score))
